9657c0e545aa63f33351d6cadb359c9461825441
Michael Kassim <master_kunle14@hotmail.co.uk>
2022-05-29 09:19:11 +0100

    organizing folder layout

--- smart_contract_workspace/contracts/1_Storage.sol
+++ smart_contract_workspace/contracts/1_Storage.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.7.0 <0.9.0;
 /**
  * @title Storage
  * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract Storage {
 
--- smart_contract_workspace/tests/4_Ballot_test.sol
+++ smart_contract_workspace/tests/4_Ballot_test.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
-import "hardhat/console.sol";
 import "../contracts/3_Ballot.sol";
 
 contract BallotTest {
@@ -16,7 +15,6 @@ contract BallotTest {
     }
     
     function checkWinningProposal () public {
-        console.log("Running checkWinningProposal");
         ballotToTest.vote(0);
         Assert.equal(ballotToTest.winningProposal(), uint(0), "proposal at index 0 should be the winning proposal");
         Assert.equal(ballotToTest.winnerName(), bytes32("candidate1"), "candidate1 should be the winner name");
